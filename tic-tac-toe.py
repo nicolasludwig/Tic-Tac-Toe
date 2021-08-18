8c2a3609b61bd7322f90990b28d1707fd41bfbcb
nicolasludwig <nicolas_ludwig@outlook.com>
2021-08-17 22:00:02 -0300

    Desenho melhorado

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,15 +1,19 @@
 import os
+import random
 
 matriz=[ [" "," "," "],
          [" "," "," "],
          [" "," "," "] ]
 
 def desenhaMatriz():
-    print("\n—————————————")
+    os.system("cls")
+    print("\nJOGO DA VELHA\n")
     for i in range(0,3):
         for j in range (0,3):
-            print("|",matriz[i][j],"",end="")
-        print("|\n—————————————")
+            if j is not 0:
+                print("|",end="")
+            print("",matriz[i][j],"",end="")
+        if i is not 2: print("\n———|———|———")
     print("\n")
 
 desenhaMatriz()
